c82ee048865501008a2908185652bcc365370556
Kenneth1234 <kenich94@gmail.com>
2022-07-05 15:07:35 +0000

    cript that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-'''task 7 module'''
+"""save_to_json_file
+"""
+import json
 
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file, using JSON
+    """
 
-arglist = list(sys.argv[1:])
-
-try:
-    old_data = load_from_json_file('add_item.json')
-except Exception:
-    old_data = []
-
-old_data.extend(arglist)
-save_to_json_file(old_data, 'add_item.json')
+    with open(filename, mode="w", encoding="UTF-8") as saveFile:
+        json.dump(my_obj, saveFile)
